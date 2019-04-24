ba16ad63726ffd1d953d5d7de6cff7b4892f754b
Jude Jesuthasan <jude.jesuthasan@watchguard.com>
2019-04-23 23:55:44 -0400

    deployment s3

--- Infra/deployment/run.py
+++ Infra/deployment/run.py
@@ -3,6 +3,35 @@ import os
 import logging
 
 
+stack_outputs = {}
+
+
+def deploy_ddb_stack(stack):
+    tmpl = open(os.path.join('../CFN', 'ddb.yaml')).read()
+    ddb_params = [
+        {'ParameterKey': 'DDBTableName', 'ParameterValue': 'ddb-stream'}
+    ]
+    stackname = 'ddb-stream'
+    stack.create_stack(stackname=stackname, template=tmpl,
+                       parameters=ddb_params)
+    stack_outputs.update(stack.stack_output(stackname))
+
+
+def deploy_s3_stack(stack):
+    tmpl = open(os.path.join('../CFN', 's3.yaml')).read()
+    ddb_params = [
+        {'ParameterKey': 'LambdaBucket', 'ParameterValue': 'lambda-bucket'}
+    ]
+    stackname = 's3-lambda'
+    stack.create_stack(stackname=stackname, template=tmpl,
+                       parameters=ddb_params)
+    stack_outputs.update(stack.stack_output(stackname))
+
+
+def deploy_lambda_stack(stack):
+    stack_outputs.update(stack.stack_output('my-network'))
+    
+
 if __name__ == '__main__':
     logging.basicConfig(
         format='%(asctime)s - %(levelname)s - %(message)s',
@@ -10,10 +39,5 @@ if __name__ == '__main__':
     )
 
     stack = cfn.CfnStack('default')
-    tmpl = open(os.path.join('../CFN', 'ddb.yaml')).read()
-    ddb_params = [
-        {'ParameterKey': 'DDBTableName', 'ParameterValue': 'ddb-stream'}
-    ]
-
-    stack.create_stack(stackname='ddb-stream', template=tmpl,
-                       parameters=ddb_params)
+    deploy_ddb_stack(stack)
+    deploy_s3_stack(stack)
